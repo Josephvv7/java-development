e4a643bdfe30dc10e00a5854df65882cd5e3739c
Josephvv7 <joseph.medina33@gmail.com>
2025-05-08 10:48:11 -0400

    Completed 2nd name formatter from exercise, now I can do the first and last one

--- workbook-4/StaticClass/src/main/java/com/pluralsight/NameFormatter.java
+++ workbook-4/StaticClass/src/main/java/com/pluralsight/NameFormatter.java
@@ -10,6 +10,24 @@ public class NameFormatter {
 
     public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
         StringBuilder builder = new StringBuilder();
+
+        builder.append(lastName).append(", ");
+
+        if (!prefix.isEmpty()) {
+            builder.append(prefix).append(" ");
+        }
+
+        builder.append(firstName);
+
+        if (!middleName.isEmpty()){
+            builder.append(" ").append(middleName);
+        }
+
+        if (!suffix.isEmpty()) {
+            builder.append(", ").append(suffix);
+        }
+
+        return builder.toString();
     }
 
     public static String format(String fullName) {
